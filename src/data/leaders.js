ca8835f75473b6d5f9a1f72ec7efba55afb55459
Falcon-Exe <muhsabir.c@gmail.com>
2026-03-08 00:55:13 +0530

    feat: add leadership data for executive and sub-committees

--- src/data/leaders.js
+++ src/data/leaders.js
@@ -1,113 +1,137 @@
 export const leadershipData = {
     executive: [
-        {
-            id: 1,
-            name: "Razin",
-            role: "Chairman",
-            image: "/leaders/razin.jpeg"
-        },
-        {
-            id: 2,
-            name: "Dilshad",
-            role: "Secretary",
-            image: "/leaders/dilshad.JPG"
-        },
-        {
-            id: 3,
-            name: "Jirshad",
-            role: "Joint Secretary",
-            image: ""
-        },
-        {
-            id: 4,
-            name: "Salman",
-            role: "Vice Chairman",
-            image: ""
-        },
-        {
-            id: 5,
-            name: "Shiyas",
-            role: "Treasurer",
-            image: ""
-        },
-        {
-            id: 6,
-            name: "Anees",
-            role: "CUC",
-            image: ""
-        }
+        { id: "e1", name: "Razin", role: "Chairman", image: "" },
+        { id: "e2", name: "Salman", role: "Vice Chairman", image: "" },
+        { id: "e3", name: "Dilshad", role: "General Secretary", image: "" },
+        { id: "e4", name: "Jirshad", role: "Joint Secretary", image: "" },
+        { id: "e5", name: "Anees Rahman", role: "CUC", image: "" },
+        { id: "e6", name: "Shiyas", role: "Treasurer", image: "" }
     ],
     subCommittees: [
         {
-            title: "Fine Arts",
+            title: "Auditors",
             members: [
-                { id: 101, name: "Minhaj", role: "Committee Member", image: "" },
-                { id: 102, name: "Razak", role: "Committee Member", image: "" }
+                { id: "au1", name: "Saed", role: "Auditor", image: "" },
+                { id: "au2", name: "Ihsan", role: "Assi - Auditor", image: "" },
+                { id: "au3", name: "Althaf", role: "Assi - Auditor", image: "" }
             ]
         },
         {
-            title: "Literary",
+            title: "Fine Arts Wing",
             members: [
-                { id: 103, name: "Shiyaf", role: "Committee Member", image: "" },
-                { id: 104, name: "Marwan", role: "Committee Member", image: "" }
+                { id: "fa1", name: "Minhaj Ap", role: "Fine Arts Co-ordinator", image: "" },
+                { id: "fa2", name: "Rasak", role: "Asst. Co-Ordinator", image: "" },
+                { id: "fa3", name: "Sahnoon", role: "Convener (CLASS 1)", image: "" },
+                { id: "fa4", name: "Saneen", role: "Convener (CLASS 2)", image: "" },
+                { id: "fa5", name: "Zahir", role: "Convener (CLASS 3)", image: "" },
+                { id: "fa6", name: "Fayis", role: "Convener (CLASS 4)", image: "" },
+                { id: "fa7", name: "Sinan ck", role: "Convener (CLASS 5)", image: "" }
             ]
         },
         {
-            title: "Library",
+            title: "Literary Wing",
             members: [
-                { id: 105, name: "Abdulla", role: "Committee Member", image: "" },
-                { id: 106, name: "Hisham", role: "Committee Member", image: "" }
+                { id: "lw1", name: "Shiyaf", role: "Literary Co-ordinator", image: "" },
+                { id: "lw2", name: "Marwan", role: "Asst Co-ordinator", image: "" },
+                { id: "lw3", name: "Jibran", role: "Chief Editor", image: "" },
+                { id: "lw4", name: "Razin", role: "Convener (CLASS 1)", image: "" },
+                { id: "lw5", name: "Anshad", role: "Convener (CLASS 2)", image: "" },
+                { id: "lw6", name: "Mujthaba", role: "Convener (CLASS 3)", image: "" },
+                { id: "lw7", name: "Shahid", role: "Convener (CLASS 4)", image: "" },
+                { id: "lw8", name: "Sinan ck", role: "Convener (CLASS 5)", image: "" }
             ]
         },
         {
-            title: "Research Cell",
+            title: "Library Board",
             members: [
-                { id: 107, name: "Miqdad", role: "Committee Member", image: "" },
-                { id: 108, name: "Faseeh", role: "Committee Member", image: "" }
+                { id: "lb1", name: "Abdulla Adil", role: "Chief Librarian", image: "" },
+                { id: "lb2", name: "Hisham", role: "Assi - Librarian", image: "" }
             ]
         },
         {
-            title: "Medical",
+            title: "Research Wing",
             members: [
-                { id: 109, name: "Basith", role: "Committee Member", image: "" },
-                { id: 110, name: "Fayiz", role: "Committee Member", image: "" }
+                { id: "rw1", name: "Miqdad", role: "Research Co-ordinator", image: "" },
+                { id: "rw2", name: "Faseeh", role: "Asst Co-ordinator", image: "" },
+                { id: "rw3", name: "Sanad", role: "Club Leader (Beacon of Truth)", image: "" },
+                { id: "rw4", name: "Sinan (4)", role: "Club Leader (Al-Madhab)", image: "" },
+                { id: "rw5", name: "Hashim", role: "Club Leader (Thahdir)", image: "" }
             ]
         },
         {
-            title: "Bank",
+            title: "Saving Bank",
             members: [
-                { id: 111, name: "Nabeel", role: "Committee Member", image: "" },
-                { id: 112, name: "Faris", role: "Committee Member", image: "" }
+                { id: "sb1", name: "Nabeel", role: "Bank Manager", image: "" },
+                { id: "sb2", name: "Faris CA", role: "Asst. Manager", image: "" }
             ]
         },
         {
-            title: "Social Affairs",
+            title: "Health Department",
             members: [
-                { id: 113, name: "Mujeeb", role: "Committee Member", image: "" },
-                { id: 114, name: "Habeeb", role: "Committee Member", image: "" }
+                { id: "hd1", name: "Basith", role: "Chief Medical Officer (CMO)", image: "" },
+                { id: "hd2", name: "FAYIS", role: "Assi - Medical Officer (AMO)", image: "" },
+                { id: "hd3", name: "Savad (C1)", role: "Member", image: "" },
+                { id: "hd4", name: "Ameen (C2)", role: "Member", image: "" },
+                { id: "hd5", name: "Yaseen (C3)", role: "Member", image: "" }
             ]
         },
         {
-            title: "Sports",
+            title: "Store Board",
             members: [
-                { id: 115, name: "Anzar", role: "Committee Member", image: "" },
-                { id: 116, name: "Hadhi", role: "Committee Member", image: "" }
+                { id: "st1", name: "Ramil PA", role: "Storekeeper", image: "" },
+                { id: "st2", name: "Hashir", role: "Assi - keeper", image: "" }
             ]
         },
         {
-            title: "Garden",
+            title: "Garden Committee",
             members: [
-                { id: 117, name: "Sinan", role: "Committee Member", image: "" },
-                { id: 118, name: "Ajsal", role: "Committee Member", image: "" }
+                { id: "gc1", name: "Sinan", role: "Garden Leader", image: "" },
+                { id: "gc2", name: "Ajsal", role: "Assi - Leader", image: "" }
             ]
         },
         {
-            title: "IT & PRD",
+            title: "Social Affairs Board",
             members: [
-                { id: 119, name: "Basil", role: "Committee Member", image: "" },
-                { id: 120, name: "Sabir", role: "Committee Member", image: "" },
-                { id: 121, name: "Afinas", role: "Committee Member", image: "" },
-                { id: 122, name: "Ashmil", role: "Committee Member", image: "" }
+                { id: "sa1", name: "Habeeb", role: "Social Affairs Co-Ordinator", image: "" },
+                { id: "sa2", name: "Mujeeb", role: "Asst Co-Ordinator", image: "" },
+                { id: "sa3", name: "Sani", role: "CSS Coordinator", image: "" },
+                { id: "sa4", name: "Mahroof", role: "CSS Coordinator", image: "" }
+            ]
+        },
+        {
+            title: "Sports Wing",
+            members: [
+                { id: "sw1", name: "Anzar", role: "Captain", image: "" },
+                { id: "sw2", name: "Hadi", role: "Asst Captain", image: "" }
+            ]
+        },
+        {
+            title: "Public Relations & IT Department",
+            members: [
+                { id: "pr1", name: "Basil km", role: "PRO", image: "" },
+                { id: "pr2", name: "Sabir", role: "APRO", image: "" },
+                { id: "pr3", name: "Nihal", role: "IT Nob", image: "" },
+                { id: "pr4", name: "Ashmil", role: "Assi - IT Nob", image: "" },
+                { id: "pr5", name: "Hashim (3)", role: "Content writing", image: "" },
+                { id: "pr6", name: "Afinas", role: "Graphic Designing", image: "" },
+                { id: "pr7", name: "Zahir", role: "Photography", image: "" },
+                { id: "pr8", name: "Ansif", role: "Report Writing", image: "" },
+                { id: "pr9", name: "Mufeed", role: "Video Editing", image: "" }
+            ]
+        },
+        {
+            title: "Language Hub",
+            members: [
+                { id: "lh1", name: "Nizamudheen", role: "Co-ordinator", image: "" },
+                { id: "lh2", name: "Hashim(3)", role: "Assi-Co-ordinator", image: "" },
+                { id: "lh3", name: "Munzir", role: "Arabic Club Leader", image: "" },
+                { id: "lh4", name: "Mikdad", role: "Arabic Club Leader", image: "" },
+                { id: "lh5", name: "Saed", role: "English Club Leader", image: "" },
+                { id: "lh6", name: "Ashmil", role: "English Club Leader", image: "" },
+                { id: "lh7", name: "Aflah E", role: "Malayalam Club Leader", image: "" },
+                { id: "lh8", name: "Shibili C", role: "Malayalam Club Leader", image: "" },
+                { id: "lh9", name: "Ashfak", role: "Urdu Club Leader", image: "" },
+                { id: "lh10", name: "Afrad", role: "Urdu Club Leader", image: "" }
             ]
         }
     ]
